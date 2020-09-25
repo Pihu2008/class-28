f88426be641efcb75f58bfe059b89e65a625ec4e
Pihu Tiwari <70630802+Pihu2008@users.noreply.github.com>
2020-09-25 13:26:35 +0530

    Add files via upload

---
+++ AngryBirdsStage2.5StudentActivity-master/chain.js
@@ -0,0 +1,22 @@
+class SlingShot{
+    constructor(bodyA,pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            length: 5,
+            stiffness: 0.01
+        }
+        this.pointB = pointB;
+     this.chain = Constraint.create(options)
+     World.add(world,this.chain)
+    }
+    fly(){
+        this.chain.bodyA=null;
+    }
+    display(){
+        if(this.chain.bodyA){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.pointB;
+        line(pointA.x,pointA.y,pointB.x,pointB.y)}
+    }
+}
